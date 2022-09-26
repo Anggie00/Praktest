8849a7ab2fc32a3f32edad7331a6fb4c0aa5fcf0
Anggie Armelia <112962745+Anggie00@users.noreply.github.com>
2022-09-26 13:52:54 +0700

    Add files via upload

---
+++ Anggie Postest2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int x;
+
+cin>>x;
+cout<< "harga setelah diskon:"<<x- (x*10/100) <<endl;
+cout<< "diskon yang di dapat:"<<x*10/100 <<endl;
+  
+  
+  
+return 0;
+  }
