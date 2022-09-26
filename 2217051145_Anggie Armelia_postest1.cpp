b625b27f0fd54ab9a8644d3f1776b47993c6ddff
Anggie Armelia <112962745+Anggie00@users.noreply.github.com>
2022-09-26 13:53:23 +0700

    Add files via upload

---
+++ 2217051145_Anggie Armelia_postest1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  int myNum = 29;
+  cout << myNum << "\n";
+  float bilangankoma=22.9;
+  cout << bilangankoma << "\n";
+  string myText = "Haii";
+  cout << myText << "\n";
+  char myLetter = 'H';
+  cout << myLetter << "\n";
+
+  return 0;
+
+  
+  
+  
+}
