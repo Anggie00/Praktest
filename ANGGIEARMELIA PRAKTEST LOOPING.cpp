56f92d5975c3c6d277ef77f37552c1662e7c3ffc
Anggie Armelia <112962745+Anggie00@users.noreply.github.com>
2022-09-29 13:15:31 +0700

    Add files via upload

---
+++ ANGGIEARMELIA PRAKTEST LOOPING.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int b,c,kelipatan;
+    cin >> b >> c;
+    for(kelipatan = b; kelipatan<= c; kelipatan = kelipatan+ b){
+    	cout << kelipatan <<endl;
+	}
+    }
